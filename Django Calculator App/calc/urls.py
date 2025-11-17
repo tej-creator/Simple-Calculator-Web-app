5dbdec06af33d846c7a2171b93ea8435afd515f9
pavan <gpawanteja@gmail.com>
2025-11-17 13:46:56 +0530

    => in urls.py with django latest version django.conf. urls has deprecated => in index.html load staticfiles no longer used so replaced with load static

--- Django Calculator App/calc/urls.py
+++ Django Calculator App/calc/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from calc import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
